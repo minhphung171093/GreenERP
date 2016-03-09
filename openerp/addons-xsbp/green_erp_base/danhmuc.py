fb358ef154d99b4ee53d970cf3ec15199ca78ad4
phuoc <your_email@example.com>
2016-03-09 16:16:59 +0700

    up

--- openerp/addons-xsbp/green_erp_base/danhmuc.py
+++ openerp/addons-xsbp/green_erp_base/danhmuc.py
@@ -39,48 +39,48 @@ class tinh_tp(osv.osv):
             
         return self.name_get(cr, user, ids, context=context)
 tinh_tp()
-# class phuong_xa(osv.osv):
-#     _name = "phuong.xa"
-#     _columns = {
-#         'name': fields.char('Phường (xã)',size = 50, required = True),
-#         'code': fields.char('Mã Phường/Xã',size = 1024, required = True),
-#         'quan_huyen_id': fields.many2one( 'quan.huyen','Quận (huyện)', required = True),
-#                 }
-#     def name_search(self, cr, user, name, args=None, operator='ilike', context=None, limit=100):
-#         if not args:
-#             args = []
-#         if context is None:
-#             context = {}
-#         if not name:
-#             ids = self.search(cr, user, args, limit=limit, context=context)
-#         else:
-#             ids = self.search(cr, user, [('code',operator,name)] + args, limit=limit, context=context)
-#             if not ids:
-#                 ids = self.search(cr, user, [('name',operator,name)] + args, limit=limit, context=context)
-#             
-#         return self.name_get(cr, user, ids, context=context)
-# phuong_xa()
-# class quan_huyen(osv.osv):
-#     _name = "quan.huyen"
-#     _columns = {
-#         'name': fields.char('Quận (huyện)',size = 50, required = True),
-#         'code': fields.char('Mã Quận/Huyện',size = 1024, required = True),
-#         'tinh_tp_id':fields.many2one('tinh.tp','Thuộc Tỉnh/Thành phố', required = True),
-#                 }
-#     def name_search(self, cr, user, name, args=None, operator='ilike', context=None, limit=100):
-#         if not args:
-#             args = []
-#         if context is None:
-#             context = {}
-#         if not name:
-#             ids = self.search(cr, user, args, limit=limit, context=context)
-#         else:
-#             ids = self.search(cr, user, [('code',operator,name)] + args, limit=limit, context=context)
-#             if not ids:
-#                 ids = self.search(cr, user, [('name',operator,name)] + args, limit=limit, context=context)
-#             
-#         return self.name_get(cr, user, ids, context=context)
-# quan_huyen()
+class phuong_xa(osv.osv):
+    _name = "phuong.xa"
+    _columns = {
+        'name': fields.char('Phường (xã)',size = 50, required = True),
+        'code': fields.char('Mã Phường/Xã',size = 1024, required = True),
+        'quan_huyen_id': fields.many2one( 'quan.huyen','Quận (huyện)', required = True),
+                }
+    def name_search(self, cr, user, name, args=None, operator='ilike', context=None, limit=100):
+        if not args:
+            args = []
+        if context is None:
+            context = {}
+        if not name:
+            ids = self.search(cr, user, args, limit=limit, context=context)
+        else:
+            ids = self.search(cr, user, [('code',operator,name)] + args, limit=limit, context=context)
+            if not ids:
+                ids = self.search(cr, user, [('name',operator,name)] + args, limit=limit, context=context)
+            
+        return self.name_get(cr, user, ids, context=context)
+phuong_xa()
+class quan_huyen(osv.osv):
+    _name = "quan.huyen"
+    _columns = {
+        'name': fields.char('Quận (huyện)',size = 50, required = True),
+        'code': fields.char('Mã Quận/Huyện',size = 1024, required = True),
+        'tinh_tp_id':fields.many2one('tinh.tp','Thuộc Tỉnh/Thành phố', required = True),
+                }
+    def name_search(self, cr, user, name, args=None, operator='ilike', context=None, limit=100):
+        if not args:
+            args = []
+        if context is None:
+            context = {}
+        if not name:
+            ids = self.search(cr, user, args, limit=limit, context=context)
+        else:
+            ids = self.search(cr, user, [('code',operator,name)] + args, limit=limit, context=context)
+            if not ids:
+                ids = self.search(cr, user, [('name',operator,name)] + args, limit=limit, context=context)
+            
+        return self.name_get(cr, user, ids, context=context)
+quan_huyen()
 
 # class res_partner(osv.osv):
 #     _inherit = "res.partner"

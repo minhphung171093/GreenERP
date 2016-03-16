4a4e653a91d1c17bad69398f5bb16e4c1c1a3a10
phuoc <your_email@example.com>
2016-03-16 08:48:51 +0700

    up

--- openerp/addons-xsbp/green_erp_phathanh/graph_report.py
+++ openerp/addons-xsbp/green_erp_phathanh/graph_report.py
@@ -29,7 +29,7 @@ class doanhthu_graph_report(osv.osv):
                     left join loai_ve lv on pptt.loai_ve_id = lv.id
                     left join ky_ve kv on pptt.ky_ve_id = kv.id
                  
-                union
+                union all
                  
                 select vel.phanphoi_line_id as id, vel.daily_id as daily_id, ve.ky_ve_id as ky_ve_id,-1*vel.thuc_kiem*lv.gia_tri as giatri, kv.ngay_mo_thuong as ngay_mo_thuong
                  
@@ -57,38 +57,40 @@ class dthu_phanh_graph_report(osv.osv):
         cr.execute("""
             create or replace view dthu_phanh_graph_report as (
 
-                select id, daily_id, ky_ve_id, sum(giatri) as giatri, loai_giatri
+                select id, daily_id, ky_ve_id, sum(giatri) as giatri, loai_giatri, ngay_mo_thuong
                 from
                 (
                 select ppttl.id as id, ppttl.daily_id as daily_id, pptt.ky_ve_id as ky_ve_id,
                     case when (select sove_sau_dc from dieuchinh_line where phanphoi_line_id=ppttl.id order by id desc limit 1) is not null
                         then (select sove_sau_dc*lv.gia_tri from dieuchinh_line where phanphoi_line_id=ppttl.id order by id desc limit 1) else ppttl.sove_kynay*lv.gia_tri end giatri,
-                    
+                    kv.ngay_mo_thuong as ngay_mo_thuong,
                     'Doanh Thu' as loai_giatri
                     from phanphoi_tt_line ppttl 
                     left join phanphoi_truyenthong pptt on ppttl.phanphoi_tt_id=pptt.id
                     left join loai_ve lv on pptt.loai_ve_id = lv.id
+                    left join ky_ve kv on pptt.ky_ve_id = kv.id
+                union all
                 
-                union
-                
-                select vel.phanphoi_line_id as id, vel.daily_id as daily_id, ve.ky_ve_id as ky_ve_id,-1*vel.thuc_kiem*lv.gia_tri as giatri,'Doanh Thu' as loai_giatri
+                select vel.phanphoi_line_id as id, vel.daily_id as daily_id, ve.ky_ve_id as ky_ve_id,-1*vel.thuc_kiem*lv.gia_tri as giatri,'Doanh Thu' as loai_giatri, 
+                kv.ngay_mo_thuong as ngay_mo_thuong
                 
                     from nhap_ve_e_line vel
                     left join nhap_ve_e ve on vel.nhap_ve_e_id=ve.id
                     left join loai_ve lv on ve.loai_ve_id = lv.id
+                    left join ky_ve kv on ve.ky_ve_id = kv.id
+                union all
 
-                union
-
-                select ppttl.id as id, ppttl.daily_id as daily_id, pptt.ky_ve_id as ky_ve_id,
+                select ppttl.id as id, ppttl.daily_id as daily_id, pptt.ky_ve_id as ky_ve_id, 
                     case when (select sove_sau_dc from dieuchinh_line where phanphoi_line_id=ppttl.id order by id desc limit 1) is not null
                         then (select sove_sau_dc*lv.gia_tri from dieuchinh_line where phanphoi_line_id=ppttl.id order by id desc limit 1) else ppttl.sove_kynay*lv.gia_tri end giatri,
+                    kv.ngay_mo_thuong as ngay_mo_thuong,
                     'Phát Hành' as loai_giatri
                     from phanphoi_tt_line ppttl 
                     left join phanphoi_truyenthong pptt on ppttl.phanphoi_tt_id=pptt.id
                     left join loai_ve lv on pptt.loai_ve_id = lv.id
-                
+                    left join ky_ve kv on ve.ky_ve_id = kv.id
                 )foo
-                group by id,daily_id,ky_ve_id,loai_giatri
+                group by id,daily_id,ky_ve_id,loai_giatri,ngay_mo_thuong
             )
         """)
 dthu_phanh_graph_report()

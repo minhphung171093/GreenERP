535af788a82614041334963afb78c0f20df1a801
phuoc <your_email@example.com>
2016-03-17 13:57:23 +0700

    up

--- openerp/addons-xsbp/green_erp_phathanh/graph_report.py
+++ openerp/addons-xsbp/green_erp_phathanh/graph_report.py
@@ -17,28 +17,31 @@ class doanhthu_graph_report(osv.osv):
         cr.execute("""
             create or replace view doanhthu_graph_report as (
  
-                select id, daily_id, ky_ve_id, sum(giatri) as giatri, ngay_mo_thuong
+                select id, daily_id, ky_ve_id, sum(giatri) as giatri, ngay_mo_thuong, diem_tra_e_id as tinh(thi_truong)
                 from
                 (
                 select ppttl.id as id, ppttl.daily_id as daily_id, pptt.ky_ve_id as ky_ve_id,
                     case when (select sove_sau_dc from dieuchinh_line where phanphoi_line_id=ppttl.id order by id desc limit 1) is not null
                         then (select sove_sau_dc*lv.gia_tri from dieuchinh_line where phanphoi_line_id=ppttl.id order by id desc limit 1) else ppttl.sove_kynay*lv.gia_tri end giatri,
-                    kv.ngay_mo_thuong as ngay_mo_thuong
+                    kv.ngay_mo_thuong as ngay_mo_thuong, vel.diem_tra_e_id as diem_tra_e_id
+                    
                     from phanphoi_tt_line ppttl 
                     left join phanphoi_truyenthong pptt on ppttl.phanphoi_tt_id=pptt.id
                     left join loai_ve lv on pptt.loai_ve_id = lv.id
                     left join ky_ve kv on pptt.ky_ve_id = kv.id
-                 
+                    left join nhap_ve_e_line vel on vel.phanphoi_line_id = ppttl.id
+                     
                 union all
                  
-                select vel.phanphoi_line_id as id, vel.daily_id as daily_id, ve.ky_ve_id as ky_ve_id,-1*vel.thuc_kiem*lv.gia_tri as giatri, kv.ngay_mo_thuong as ngay_mo_thuong
-                 
+                select vel.phanphoi_line_id as id, vel.daily_id as daily_id, ve.ky_ve_id as ky_ve_id,-1*vel.thuc_kiem*lv.gia_tri as giatri, kv.ngay_mo_thuong as ngay_mo_thuong,
+                    vel.diem_tra_e_id as diem_tra_e_id
+                    
                     from nhap_ve_e_line vel
                     left join nhap_ve_e ve on vel.nhap_ve_e_id=ve.id
                     left join loai_ve lv on ve.loai_ve_id = lv.id
                     left join ky_ve kv on ve.ky_ve_id = kv.id
                 )foo
-                group by id,daily_id,ky_ve_id,ngay_mo_thuong
+                group by id,daily_id,ky_ve_id,ngay_mo_thuong,diem_tra_e_id
             )
         """)
 doanhthu_graph_report()
